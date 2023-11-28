6327c62cc481a4e323c9085d692096facc800821
rlarlqja1001 <rlarlqja1001@gmail.com>
2023-11-28 08:15:21 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,4 +1,14 @@
+resource "azurerm_resource_group" "myresourcegroup" {
+  name     = "${var.prefix}-workshop"
+  location = var.location
+
+  tags = {
+    environment = "Production"
+  }
+}
+
 module "network" {
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
   source  = "app.terraform.io/gbkim-azure/network/azurerm"
   version = "3.5.0"
   # insert required variables here
